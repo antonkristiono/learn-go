630ce301bd09e6a68bcecd44b170c89124e60a9c
Anton Kristiono <antonkristiono47@gmail.com>
2021-05-24 13:18:48 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module be-superkidz
+
+go 1.16
+
+require (
+	github.com/labstack/echo/v4 v4.3.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+	golang.org/x/tools/gopls v0.6.11 // indirect
+)
---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+	"simpleapi/handlers"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
